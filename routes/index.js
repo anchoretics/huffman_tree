4c6bb3544f085bad5de00f06c18bf290a8f40141
令锋 梁 <lingfengliang@lingfengdeMacBook-Air.local>
2015-02-09 16:42:57 +0800

    i

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',author: 'anchoretic@vip.qq.com' });
 });
-
+router.get('/help',function(req, res, next){
+  console.log(req.xhr);
+  res.render('help/help',{ title: 'Help Title',author: ''});
+});
+router.get('/help/:id?',function(req, res, next){
+  console.dir(req.path);
+  res.send(req.path);
+});
 module.exports = router;
