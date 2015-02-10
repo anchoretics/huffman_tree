e38fcfeb45532018e6f41828978f04f0ce94f2fd
令锋 梁 <lingfengliang@lingfengdeMacBook-Air.local>
2015-02-10 17:40:50 +0800

    modifiy bootstrap ref

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var retData = { title: '哈夫曼编码',author: 'margarate' };
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '哈夫曼编码',author: 'margarate' });
+  res.render('index', retData);
+});
+
+router.post('/', function(req, res, next) {
+	console.dir(req.params.len);
+    res.render('index', retData);
 });
 
 module.exports = router;
