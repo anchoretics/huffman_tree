14237aa5e1d98bb87aee134cbc72bc9b461dfc33
令锋 梁 <lingfengliang@lingfengdeMacBook-Air.local>
2015-02-12 19:21:16 +0800

    move encode/decode module to controllers

---
+++ controllers/huffman/huffman.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var huffman = {
+	//
+	encode: function(req, res){
+		//if posted encodeString is not null,then do Encoding & send to Client
+		if(req.body.encodeString){
+			var encodeString = req.body.encodeString;
+			//set HTTP Header,then u can send encrypt string as a file named 'encode.txt', in this demo
+			res.set({
+				'Content-Type': 'text/plain',
+				'Content-Disposition': 'attachment; filename="encode.txt"'
+			});
+			//encrypt string here,such as :
+			encodeString = Encrypt(encodeString);
+			//send encoded string
+			res.send(encodeString);
+		}else{
+			res.redirect('/');
+		}
+	},
+	//
+	decode: function(req, res){
+		if(req.files.encodeFile){
+			console.dir(req.files);
+			var originalname = req.files.encodeFile.originalname;
+			var path = req.files.encodeFile.path;
+			res.set({
+				'Content-Type': 'text/plain',
+				'Content-Disposition': 'attachment; filename="decode.txt"'
+			});
+			//read posted file and Decrypt file's content
+			fs.readFile(path, function (error, data) {
+				if (error) res.render('error',{error: error,message: 'Error happend when decode :'});
+				//decrypt data here
+				data = Decrypt(data);
+				//send decrypted data
+				res.send(data);
+			});
+		}else{
+	    	res.redirect('/');
+		}
+	}
+};
+//Encrypt string
+function Encrypt($data){
+	//impl encrypt 
+	//...
+	return '--encoded--\n' + $data;
+}
+//Decrypt string
+function Decrypt($data){
+	//impl decrypt 
+	//...
+	return '--decoded--\n' + $data;
+}
+module.exports = huffman;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var myAbout = require('../controllers/about.js');
-var fs = require('fs');
+var huffman = require('../controllers/huffman/huffman.js');
 
 var router = express.Router();
 
@@ -16,38 +16,10 @@ router.get('/', function(req, res, next) {
 });
 
 //Deal posted encoding form
-router.post('/Encoding', function(req, res, next) {
-	//if posted encodeString is not null,then do Encoding & send to Client
-	if(req.body.encodeString){
-		var encodeString = req.body.encodeString;
-		//set HTTP Header,then u can send string as a file named 'encode.txt', in this demo
-		res.set({
-			'Content-Type': 'text/plain',
-			'Content-Disposition': 'attachment; filename="encode.txt"'
-		});
-		//encode string
-		//encodeString = Encypt(encodeString);
-		//send encoded string
-		res.send(encodeString);
-		retData.encodeString = encodeString;
-	}else{
-		retData.encodeString = '';
-		res.redirect('/');
-	}
-
-});
+router.post('/Encoding', huffman.encode);
 
 //Deal posted decoding form
-router.post('/Decoding', function(req, res, next) {
-	if(req.files.encodeFile){
-		console.dir(req.files);
-		var originalname = req.files.encodeFile.originalname;
-		var path = req.files.encodeFile.path;
-
-	}else{
-    	res.render('index', retData);
-	}
-});
+router.post('/Decoding', huffman.decode);
 
 //add about module
 router.get('/about',myAbout.about);
